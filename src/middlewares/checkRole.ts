5bda300c7f05e3d183c1ea514da136f953ea86f0
Hendar <hendarsundana@gmail.com>
2021-11-24 06:00:56 +0700

    fix call class

--- src/middlewares/checkRole.ts
+++ src/middlewares/checkRole.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { getRepository } from "typeorm";
 
-import { User } from "../entity/User";
+import { Users } from "../entity/User";
 
 export const checkRole = (roles: Array<string>) => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -9,8 +9,8 @@ export const checkRole = (roles: Array<string>) => {
         const id = res.locals.jwtPayload.userId;
 
         //Get user role from the database
-        const userRepository = getRepository(User);
-        let user: User;
+        const userRepository = getRepository(Users);
+        let user: Users;
         try {
             user = await userRepository.findOneOrFail(id);
         } catch (id) {
--- src/migration/1637590555908-User.ts
+++ src/migration/1637590555908-User.ts
@@ -1,15 +1,15 @@
 import {getRepository, MigrationInterface, QueryRunner} from "typeorm";
-import { User } from "../entity/User";
+import { Users } from "../entity/User";
 
 export class User1637590555908 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        let user = new User();
+        let user = new Users();
         user.fullname = "ADMIN";
         user.email = "admin@mail.com";
         user.password = "admin@123!";
         user.hashPassword();
-        const userRepository = getRepository(User);
+        const userRepository = getRepository(Users);
         await userRepository.save(user);
     }
 
